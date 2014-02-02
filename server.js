bd58de1b4111e558e29d04cc2bb945d232fa8ba6
vagrant <vagrant@precise32.(none)>
2014-02-02 21:05:53 +0000

    Added server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+var port = 8000;
+app.listen(port);
+console.log("Express app listening on port " + port);
+
+app.get('/', function(req, res) {
+	res.send('Hellow, welcome to CS1501')
+
+});
